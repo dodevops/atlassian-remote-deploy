92d04648d4660548941b3b3c0815dd97b4de601b
Dennis Ploeger <dennis.ploeger@kps.com>
2018-05-28 16:57:00 +0200

    Fixe

--- lib/install/AbstractInstaller.ts
+++ lib/install/AbstractInstaller.ts
@@ -2,5 +2,5 @@ import { InstallCommandOptions } from '../commands/install'
 import Bluebird = require('bluebird')
 
 export abstract class AbstractInstaller {
-  public abstract install(options: InstallCommandOptions): Bluebird<void>
+  public abstract install (options: InstallCommandOptions): Bluebird<void>
 }
